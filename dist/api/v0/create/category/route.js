1b1964102ee8b743f19badc1662ddb637460e136
پوریا موسوی <super.pra2017@gmail.com>
2023-09-05 16:53:00 +0330

    develop

---
+++ dist/api/v0/create/category/route.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mod = [
+    {
+        checkout: (context) => {
+            const bd = context.body.data;
+            if (!bd || !bd.name || !bd.info || !bd.meta || !bd.imgs)
+                return "no body";
+            return true;
+        },
+        dbset: (context, helpers) => {
+            const bd = context.body.data;
+            return {
+                sql: `INSERT INTO category(_id,name,info,meta,imgs,_by) VALUES($1,$2,$3,$4,$5,$6)`,
+                value: ["CG" + helpers.getRandomContinusId(), bd.name, bd.info, bd.meta, bd.imgs, 'dev'],
+                next(res) {
+                    return res;
+                },
+                onError(err) {
+                    return err.message;
+                },
+            };
+        },
+        response: (context) => {
+            return {
+                done: !!context.action.dbset.rowCount,
+                "data-provider": {},
+            };
+        },
+    },
+];
+exports.default = mod;
